d2d8c299af111119bb75452d34e5bf02a3f4791b
nikopol <levichev.dmitry@gmail.com>
2020-11-17 13:23:40 +0300

    Add GRPC cmd service

--- service-beacons-node/obj/src/clients/version1/index.js
+++ service-beacons-node/obj/src/clients/version1/index.js
@@ -6,4 +6,6 @@ var BeaconsHttpClientV1_1 = require("./BeaconsHttpClientV1");
 Object.defineProperty(exports, "BeaconsHttpClientV1", { enumerable: true, get: function () { return BeaconsHttpClientV1_1.BeaconsHttpClientV1; } });
 var BeaconsNullClientV1_1 = require("./BeaconsNullClientV1");
 Object.defineProperty(exports, "BeaconsNullClientV1", { enumerable: true, get: function () { return BeaconsNullClientV1_1.BeaconsNullClientV1; } });
+var BeaconsCommandableGrpcClientV1_1 = require("./BeaconsCommandableGrpcClientV1");
+Object.defineProperty(exports, "BeaconsCommandableGrpcClientV1", { enumerable: true, get: function () { return BeaconsCommandableGrpcClientV1_1.BeaconsCommandableGrpcClientV1; } });
 //# sourceMappingURL=index.js.map
--- service-beacons-node/src/clients/version1/index.ts
+++ service-beacons-node/src/clients/version1/index.ts
@@ -2,3 +2,4 @@ export { BeaconsDirectClientV1 } from "./BeaconsDirectClientV1";
 export { BeaconsHttpClientV1 } from "./BeaconsHttpClientV1";
 export { BeaconsNullClientV1 } from "./BeaconsNullClientV1";
 export { IBeaconsClientV1 } from "./IBeaconsClientV1";
+export {BeaconsCommandableGrpcClientV1} from "./BeaconsCommandableGrpcClientV1";
