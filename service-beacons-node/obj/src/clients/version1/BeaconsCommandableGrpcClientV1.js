3ac079e9e4ec84cb2bea21ec15e5a4e3c43ed170
nikopol <levichev.dmitry@gmail.com>
2020-11-17 13:25:07 +0300

    Add GRPC cmd client

---
+++ service-beacons-node/obj/src/clients/version1/BeaconsCommandableGrpcClientV1.js
@@ -0,0 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BeaconsCommandableGrpcClientV1 = void 0;
+const pip_services3_grpc_node_1 = require("pip-services3-grpc-node");
+class BeaconsCommandableGrpcClientV1 extends pip_services3_grpc_node_1.CommandableGrpcClient {
+    constructor() {
+        super('beacons_v1');
+        this._blockCount = 1;
+        this._ids = null;
+    }
+    configure(config) {
+        super.configure(config);
+        this._blockCount = config.getAsIntegerWithDefault("options.block_count", this._blockCount);
+    }
+    getBeacons(correlationId, filter, paging, callback) {
+        this.callCommand('get_beacons', correlationId, { filter: filter, paging: paging }, callback);
+    }
+    getBeaconById(correlationId, beaconId, callback) {
+        this.callCommand('get_beacon_by_id', correlationId, {
+            beacon_id: beaconId
+        }, callback);
+    }
+    getBeaconByUdi(correlationId, udi, callback) {
+        this.callCommand('get_beacon_by_udi', correlationId, {
+            udi: udi
+        }, callback);
+    }
+    calculatePosition(correlationId, siteId, udis, callback) {
+        this.callCommand('calculate_position', correlationId, {
+            site_id: siteId,
+            udis: udis
+        }, callback);
+    }
+    createBeacon(correlationId, beacon, callback) {
+        this.callCommand('create_beacon', correlationId, {
+            beacon: beacon
+        }, callback);
+    }
+    updateBeacon(correlationId, beacon, callback) {
+        this.callCommand('update_beacon', correlationId, {
+            beacon: beacon
+        }, callback);
+    }
+    deleteBeaconById(correlationId, beaconId, callback) {
+        this.callCommand('delete_beacon_by_id', correlationId, {
+            beacon_id: beaconId
+        }, callback);
+    }
+}
+exports.BeaconsCommandableGrpcClientV1 = BeaconsCommandableGrpcClientV1;
+//# sourceMappingURL=BeaconsCommandableGrpcClientV1.js.map
---
+++ service-beacons-node/src/clients/version1/BeaconsCommandableGrpcClientV1.ts
@@ -0,0 +1,105 @@
+import { ConfigParams, DataPage, FilterParams, PagingParams } from 'pip-services3-commons-node';
+import { CommandableGrpcClient } from 'pip-services3-grpc-node';
+import { BeaconV1 } from '../..';
+
+import { IBeaconsClientV1 } from './IBeaconsClientV1';
+
+export class BeaconsCommandableGrpcClientV1 extends CommandableGrpcClient implements IBeaconsClientV1 {
+    private _blockCount: number = 1;
+    private _ids: string[] = null;
+
+    public constructor() {
+        super('beacons_v1');
+    }
+
+    public configure(config: ConfigParams): void {
+        super.configure(config);
+
+        this._blockCount = config.getAsIntegerWithDefault("options.block_count", this._blockCount);
+    }
+
+    public getBeacons(correlationId: string, filter: FilterParams, paging: PagingParams,
+        callback: (err: any, page: DataPage<BeaconV1>) => void): void {
+        this.callCommand(
+            'get_beacons',
+            correlationId,
+            { filter: filter, paging: paging },
+            callback
+        );
+    }
+
+    public getBeaconById(correlationId: string, beaconId: string,
+        callback: (err: any, beacon: BeaconV1) => void): void {
+            
+        this.callCommand(
+            'get_beacon_by_id',
+            correlationId,
+            {
+                beacon_id: beaconId
+            },
+            callback
+        );
+    }
+
+    public getBeaconByUdi(correlationId: string, udi: string,
+        callback: (err: any, beacon: BeaconV1) => void): void {
+        this.callCommand(
+            'get_beacon_by_udi',
+            correlationId,
+            {
+                udi: udi
+            },
+            callback
+        );
+    }
+
+    public calculatePosition(correlationId: string, siteId: string, udis: string[], 
+        callback: (err: any, position: any) => void): void {
+        this.callCommand(
+            'calculate_position',
+            correlationId,
+            {
+                site_id: siteId,
+                udis: udis
+            },
+            callback
+        );    
+    }
+
+    public createBeacon(correlationId: string, beacon: BeaconV1,
+        callback: (err: any, beacon: BeaconV1) => void): void {
+        this.callCommand(
+            'create_beacon',
+            correlationId,
+            {
+                beacon: beacon
+            },
+            callback
+        );
+    }
+
+    public updateBeacon(correlationId: string, beacon: BeaconV1,
+        callback: (err: any, beacon: BeaconV1) => void): void {
+        this.callCommand(
+            'update_beacon',
+            correlationId,
+            {
+                beacon: beacon
+            },
+            callback
+        );    
+    }
+
+    public deleteBeaconById(correlationId: string, beaconId: string,
+        callback: (err: any, beacon: BeaconV1) => void): void {
+        this.callCommand(
+            'delete_beacon_by_id',
+            correlationId,
+            {
+                beacon_id: beaconId
+            },
+            callback
+        );
+    }
+  
+}
