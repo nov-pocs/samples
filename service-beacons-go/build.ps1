1f1ac1673dbb209c420ffa7abd557aaa4c71c37f
Sergey Seroukhov <seroukhov@gmail.com>
2020-11-17 07:00:22 -0700

    Update build.ps1

--- service-beacons-go/build.ps1
+++ service-beacons-go/build.ps1
@@ -5,13 +5,6 @@ $ErrorActionPreference = "Stop"
 
 # Generate image and container names using the data in the "component.json" file
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-
-# Get buildnumber from github actions
-if ($env:GITHUB_RUN_NUMBER -ne $null) {
-    $component.build = $env:GITHUB_RUN_NUMBER
-    Set-Content -Path "component.json" -Value $($component | ConvertTo-Json)
-}
-
 $buildImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
 $container=$component.name
 
@@ -20,15 +13,6 @@ if (Test-Path "./dist") {
     Remove-Item -Recurse -Force -Path "./dist/*"
 } else {
     New-Item -ItemType Directory -Force -Path "./dist"
-}
-
-# Copy private keys to access git repo
-if (-not (Test-Path -Path "docker/id_rsa")) {
-    if ($env:GIT_PRIVATE_KEY -ne $null) {
-        Set-Content -Path "docker/id_rsa" -Value $env:GIT_PRIVATE_KEY
-    } else {
-        Copy-Item -Path "~/.ssh/id_rsa" -Destination "docker"
-    }
 }
 
 # Build docker image
